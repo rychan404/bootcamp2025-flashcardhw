84efd5163d2bf22a9e08ba99271235964f8886b6
rychan404 <173099078+rychan404@users.noreply.github.com>
2025-10-27 16:26:48 -0400

    Implemented displayCard() method

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ const flashcards = [
 ];
 
 // You can use flashcards.length to get the length of the array
-
+const numCards = flashcards.length;
 // These two variables will come in handy
-let currentIndex = 0;
+let currentIndex = 0; 
 let showingTerm = true;
+
+const content = document.getElementById('card-content');
 
 // Start with this function to simply display the card
 function displayCard() {
-
+    content.innerText = `${flashcards[currentIndex].term}\n${flashcards[currentIndex].definition}`;
 }
 
 // The rest of the code you will write is apart of event listeners
+
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
